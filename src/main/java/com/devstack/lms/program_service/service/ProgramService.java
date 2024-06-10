db82a8c744ff1138453eb04aa42a1a65a6d548ae
salman <salmannisthar123@gmail.com>
2024-06-10 23:05:07 +0530

    project service create

---
+++ src/main/java/com/devstack/lms/program_service/service/ProgramService.java
@@ -0,0 +1,9 @@
+package com.devstack.lms.program_service.service;
+
+import com.devstack.lms.program_service.dto.requestDto.RequestProgramDto;
+
+public interface ProgramService {
+
+
+    public void  createProgram(RequestProgramDto programDto);
+}
---
+++ src/main/java/com/devstack/lms/program_service/service/impl/ProgramServiceImpl.java
@@ -0,0 +1,14 @@
+package com.devstack.lms.program_service.service.impl;
+
+import com.devstack.lms.program_service.dto.requestDto.RequestProgramDto;
+import com.devstack.lms.program_service.service.ProgramService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ProgramServiceImpl implements ProgramService {
+    @Override
+    public void createProgram(RequestProgramDto programDto) {
+
+    }
+}
