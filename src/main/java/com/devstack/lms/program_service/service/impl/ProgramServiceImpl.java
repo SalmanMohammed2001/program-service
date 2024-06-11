768981f0ba80d46bf45ebc7487e0935d1d8e6243
salman <salmannisthar123@gmail.com>
2024-06-12 00:11:42 +0530

    webflux to check subject id

--- src/main/java/com/devstack/lms/program_service/service/impl/ProgramServiceImpl.java
+++ src/main/java/com/devstack/lms/program_service/service/impl/ProgramServiceImpl.java
@@ -7,8 +7,12 @@ import com.devstack.lms.program_service.repo.ProgramRepository;
 import com.devstack.lms.program_service.service.ProgramService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -17,6 +21,7 @@ public class ProgramServiceImpl implements ProgramService {
 
 
     private final ProgramRepository programRepository;
+    private final WebClient webClient;
 
     @Override
     public void createProgram(RequestProgramDto programDto) {
@@ -31,11 +36,24 @@ public class ProgramServiceImpl implements ProgramService {
         //get All subject and need check if there are available or not
 
 
-     List<Long> ids= program.getSubjects().stream().map(Subject::getId).toList();
-     
+      //  List<Long> ids = program.getSubjects().stream().map(Subject::getId).toList();
+        List<Long> list=new ArrayList<>();
+        for (Subject sub:
+             program.getSubjects()) {
+            list.add(sub.getId());
+        }
 
 
+        String ids= list.stream().map(Object::toString).collect(Collectors.joining(", "));
+       Boolean isOk=  webClient.get().uri("http://localhost:8082/api/v1/subjects/{id}",ids)
+                .retrieve().bodyToMono(Boolean.class).block();
 
-        programRepository.save(program);
+       if (Boolean.TRUE.equals(isOk)){
+           programRepository.save(program);
+       }else {
+           throw new IllegalArgumentException("Try Again with Valid subject");
+       }
+
+
     }
 }
